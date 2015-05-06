a91420b8f43359ca5487a2d6e713b73865d10dcf
Igor Shevchenko <cyberskunk@gmail.com>
2015-05-06 22:36:03 +0300

    refactor and fix color info block displaying

--- static/info.js
+++ static/info.js
@@ -1,42 +1,51 @@
 var infoViewer = {
         template: document.querySelector('#template'),
-        update: function (data) {
-            var oldInfoBlock = document.querySelector('body > .color-info'),
-                color = data.color,
-                colorData = data.instances,
-                infoBlock = this.template.querySelector('.color-info').cloneNode(true),
-                instanceTemplate = this.template.querySelector('.instance').cloneNode(true);
+        instanceTemplate: document.querySelector('#instance-template'),
+        selectorTemplate: document.querySelector('#selector-template'),
+        removeOldInfoBlock: function () {
+            this.infoBlock && document.body.removeChild(this.infoBlock);
+            this.infoBlock = null;
+        },
+        _displayColor: function (color) {
+            this.infoBlock.querySelector('.color').textContent = color.toUpperCase();
+        }, 
+        _displayInstances: function (instances) {
+            var instancesWrapper = this.infoBlock.querySelector('.instances');
 
-            // remove old info block
-            oldInfoBlock && document.body.removeChild(oldInfoBlock);
-            infoBlock.removeChild(infoBlock.querySelector('.instance'));
-
-            // render color value
-            infoBlock.querySelector('.color').textContent = color.toUpperCase();
-
-            // render instances
-            colorData.forEach(function (item) {
-                var instanceNode = instanceTemplate.cloneNode(true),
-                    ruleNode = instanceNode.querySelector('.rule'),
-                    selectorNode = instanceNode.querySelector('.rule__selector');
-
-                // display first selector
-                selectorNode.textContent = item.selectors[0];
-                // display more selectors, if present
-                item.selectors.slice(1).forEach(function (selector, i) {
-                    var _node = selectorNode.cloneNode();
-
-                    _node.textContent = selector + i;
-                    instanceNode.insertBefore(_node, ruleNode.nextSibling);
-                });
+            instances.forEach(function (item) {
+                var instanceNode = infoViewer.instanceTemplate.querySelector('.instance').cloneNode(true),
+                    selectorsWrapper = instanceNode.querySelector('.selectors');
 
                 instanceNode.querySelector('.file__name').textContent = item.fileName;
                 instanceNode.querySelector('.file__line-number').textContent = item.lineNumber;
+
+                item.selectors.forEach(function (selector) {
+                    var _node = infoViewer.selectorTemplate.querySelector('.rule__selector').cloneNode(true);
+
+                    _node.textContent = selector;
+                    selectorsWrapper.appendChild(_node);
+                });
+
                 instanceNode.querySelector('.property').textContent = item.origProp;
                 instanceNode.querySelector('.value').textContent = item.origValue;
-                infoBlock.appendChild(instanceNode);
+                infoViewer.infoBlock.appendChild(instanceNode);
             });
-            document.body.insertBefore(infoBlock, colorList);
+        },
+        appendInfoBlock: function (data) {
+            if (this.infoBlock) {
+                throw new Error('infoBlock already present');
+            }
+
+            this.infoBlock = this.template.querySelector('.color-info').cloneNode(true);
+
+            this._displayColor(data.color);
+            this._displayInstances(data.instances);
+
+            document.body.insertBefore(this.infoBlock, colorList);
+        },
+        update: function (data) {
+            this.removeOldInfoBlock();
+            this.appendInfoBlock(data);
         }
     },
     colorList = document.querySelector('.color-list');
