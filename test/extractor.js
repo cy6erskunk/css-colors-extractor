8178ab4251a6d248e6014c72e96a036e83a44862
Igor Shevchenko <cyberskunk@gmail.com>
2015-10-21 11:41:01 +0300

    improve color selection, improve tests

--- test/extractor.js
+++ test/extractor.js
@@ -21,19 +21,52 @@ exports.invalidCssData = {
     throwsError: function (test) {
         test.throws(function () { extractor('xxxx---====yyy'); });
         test.done();
+    },
+    doesNotThrow: function (test) {
+        test.doesNotThrow(function () { extractor('xxx{padding:0;}'); });
+        test.doesNotThrow(function () { extractor('xxx{border:none;}'); });
+        test.done();
     }
 };
 
-var validData = '.xxx{color:red;}\n#yyy{background-color: navy}';
+var simpleValidData = '.xxx{color:#000;}';
 
 exports.validCssData = {
     noError: function (test) {
-        test.doesNotThrow(function () { extractor(validData); });
+        test.doesNotThrow(function () { extractor(simpleValidData); });
+
+        test.done();
+    },
+    noImplicitAccumulation: function (test) {
+        var result;
+        result = extractor(simpleValidData);
+        result = extractor(simpleValidData);
+
+        test.equal(Object.keys(result).length, 1);
+        test.equal(Object.keys(result).reduce(function (prev, curr) {
+            return prev + result[curr].instances.length;
+        }, 0), 1);
+        test.done();
+    },
+    explicitAccumulation: function (test) {
+        var result;
+        result = extractor('.xxx{color:#000;}'); // 1
+        result = extractor('.xxx{background-color:#111;}', result); // 2
+
+        test.equal(Object.keys(result).length, 2);
         test.done();
     },
     colorsCount: function (test) {
-        var result = extractor(validData);
-        test.equal(Object.keys(result).length, 2);
+        var result;
+        result = extractor('.xxx {color:#000;}'); // 1
+        result = extractor('.xxx {background-color:#111;}', result); // 2
+        result = extractor('.xxx {border-color:#222;}', result); // 3
+        result = extractor('.xxx {background:#333;}', result); // 4
+        result = extractor('.xxx {border:1px solid #444;}', result); // 5
+        result = extractor('.xxx {box-shadow: 0 0 0 0 #555;}', result); // 6
+        result = extractor('.xxx {border-bottom: 1px solid #666;}', result); // 7
+        test.equal(Object.keys(result).length, 7);
+
         test.done();
     }
 
